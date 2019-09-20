91610af1e5ad8119497672716ed3007b6d9fb930
Boltmerz <daoud_boulad@hotmail.com>
2019-09-20 11:58:48 +0300

    renamed var

--- src/routes/hash.js
+++ src/routes/hash.js
@@ -1,9 +1,10 @@
-import hash from 'object-hash';
+import Hash from 'object-hash';
 
 const hashUser = async (req, res) => {
   try {
     const { body } = req;
-    res.status(200).json({ hash: hash(body) });
+    const hash = Hash(body);
+    res.status(200).json({ hash });
   } catch (error) {
     res.status(500).json({ error });
   }
